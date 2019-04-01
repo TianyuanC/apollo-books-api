c1bf6d7ed015e3351c0a6ac40d2e3e89f276c78c
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-01 01:40:57 -0700

    use books api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { ApolloServer, gql } = require("apollo-server");
-
+import { ApolloServer, gql } from "apollo-server";
+import { getAll } from "./BooksAPI.js";
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
@@ -36,17 +36,11 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books
+        books: () => getAll()
     }
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
